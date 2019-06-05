e35deb69b08b4ee1dab936fc9ef26e113f07beb0
username <adisco4420@gmail.com>
2019-06-05 23:28:06 +0100

    import slide to app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from './components/core/header';
+import Slider from './components/core/slider';
 
 
 function App() {
   return (
     <div className="App">
      <Header />
+     <Slider />
     </div>
   );
 }
