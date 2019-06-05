9a6c666e0870b3dd5bae9d474868c641d062f4cb
username <adisco4420@gmail.com>
2019-06-05 21:55:22 +0100

    link to bootstrap stylesheet

---
+++ src/components/core/header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+const Header = () => {
+    return ( <nav className="navbar navbar-expand-lg navbar-light bg-danger text-light">
+    <a className="navbar-brand text-light" href="/">
+      Val Memoirs
+    </a>
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link text-light" href="/">
+            Home <span className="sr-only">(current)</span>
+          </a>
+        </li>
+      </ul>
+      <div className="my-2 my-lg-0">
+        <button
+          type="button"
+          data-toggle="modal"
+          data-target="#exampleModalCenter"
+          className="btn btn-light btn-sm"
+        >
+          <i className="fa fa-plus-circle" /> Add Story
+        </button>
+        <button data-toggle="modal" data-target="#userGuide"
+         className="btn text-light">
+          <i className="fa fa-lightbulb" /> User Guide
+        </button>
+      </div>
+    </div>
+ 
+  </nav>
+);
+}
+ 
+export default Header;
